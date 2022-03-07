b9a7b18bda81ea66d8b8cc1d1b5d89a97d514d98
John C. Westmoreland <john@westmorelandengineering.com>
2022-03-07 04:19:14 -0800

    Added in code from forked RM3100 repository --- jcw

--- Rev1/GPS-NMEA-Display/GPS-NMEA-Display.ino
+++ Rev1/GPS-NMEA-Display/GPS-NMEA-Display.ino
@@ -98,9 +98,30 @@
 #define i2cenPin 8   // low to use SPI
 #define datardyPin 9
 
+//pin definitions
+#define PIN_DRDY 9 //Set pin D9 to be the Data Ready Pin
+#define PIN_CS 10 //Chip Select (SS) is set to Pin 10
+
+//internal register values without the R/W bit
+#define RM3100_REVID_REG 0x36 // Hexadecimal address for the Revid internal register
+#define RM3100_POLL_REG 0x00 // Hexadecimal address for the Poll internal register
+#define RM3100_CMM_REG 0x01 // Hexadecimal address for the Continuous Measurement Mode internal register
+#define RM3100_STATUS_REG 0x34 // Hexadecimal address for the Status internal register
+#define RM3100_CCX1_REG 0x04 // Hexadecimal address for the Cycle Count X1 internal register
+#define RM3100_CCX0_REG 0x05 // Hexadecimal address for the Cycle Count X0 internal register
+
+//options
+#define initialCC 200 // Set the cycle count
+#define singleMode 0 //0 = use continuous measurement mode; 1 = use single measurement mode
+#define useDRDYPin 1 //0 = not using DRDYPin ; 1 = using DRDYPin to wait for data
+
+uint8_t revid;
+uint16_t cycleCount;
+float gain;
+
 #ifdef USING_HCMS_DISPLAY
-#include "RM3100.h"
-RM3100 rm3100;
+// #include "RM3100.h"
+// RM3100 rm3100;
 #endif
 
 // uncomment following if using the Lumex LDM-6432 RGB Display
@@ -571,14 +592,16 @@ if (IMU.gyroscopeAvailable()) {
 // #define csPin 10      // SPI CS
 // #define i2cenPin 8   // low to use SPI  
 // pinMode(csPin, OUTPUT); 
- pinMode(i2cenPin, OUTPUT); 
- pinMode(datardyPin, INPUT);
+ // pinMode(i2cenPin, OUTPUT); 
+ // pinMode(datardyPin, INPUT);
 // attachInterrupt(digitalPinToInterrupt(datardyPin), dataRDYIRQ, RISING);
 
 #if USE_IMU_CODE
  TempZero.init();
 #endif
-
+  pinMode(PIN_DRDY, INPUT);  
+  pinMode(PIN_CS, OUTPUT);
+  digitalWrite(PIN_CS, HIGH);
 #ifdef USING_HCMS_DISPLAY
 // analogOutputInit();
 // t = readInternalTemperature();
@@ -592,20 +615,51 @@ if (IMU.gyroscopeAvailable()) {
 #endif
 // whd_print_logbuffer();
 // debugBreak();  
-digitalWrite(i2cenPin, LOW);
+// digitalWrite(i2cenPin, LOW);
 delay(10);
 SPI.begin();
 // SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
 SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
 // SPI.beginTransaction(SPISettings(800000, MSBFIRST, SPI_MODE0));
+delay(100);
     //Set sample rate
-    rm3100.SetSampleRateReg(1); //in Hz
-    rm3100.RunCMM(1);
-attachInterrupt(digitalPinToInterrupt(datardyPin), dataRDYIRQ, RISING);    
+//    rm3100.SetSampleRateReg(1); //in Hz
+//    rm3100.RunCMM(1);
+/// /// attachInterrupt(digitalPinToInterrupt(datardyPin), dataRDYIRQ, RISING);    
 //    rm3100.SelfTest();
    //Clear Interrupt first
-    rm3100.ClearDrdyInt();      
+///     rm3100.ClearDrdyInt();      
 // SPI.endTransaction();
+
+ revid = readReg(RM3100_REVID_REG);
+  
+  Serial.print("REVID ID = 0x"); //REVID ID should be 0x22
+  Serial.println(revid, HEX);
+
+  changeCycleCount(initialCC); //change the cycle count; default = 200 (lower cycle count = higher data rates but lower resolution)
+
+  cycleCount = readReg(RM3100_CCX1_REG);
+  cycleCount = (cycleCount << 8) | readReg(RM3100_CCX0_REG);
+
+  Serial.print("Cycle Counts = "); //display cycle count
+  Serial.println(cycleCount);
+
+  gain = (0.3671 * (float)cycleCount) + 1.5; //linear equation to calculate the gain from cycle count
+
+  Serial.print("Gain = "); //display gain; default gain should be around 75 for the default cycle count of 200
+  Serial.println(gain);
+
+  if (singleMode){
+    //set up single measurement mode
+    writeReg(RM3100_CMM_REG, 0);
+    writeReg(RM3100_POLL_REG, 0x70);
+  }
+  else{
+    // Enable transmission to take continuous measurement with Alarm functions off
+    writeReg(RM3100_CMM_REG, 0x79);
+  }
+
+
 } // end setup
 
 // this is the main even loop for the Arduino project
@@ -615,7 +669,10 @@ void loop()
   int temp;
   unsigned char ones, tens = 0;
   static unsigned long counter_temp_disp;
- 
+   long x = 0;
+  long y = 0;
+  long z = 0;
+  uint8_t x2,x1,x0,y2,y1,y0,z2,z1,z0;
 
   char c, b, a = 0;
  static unsigned char counter = 0;
@@ -2141,8 +2198,20 @@ if (IMU.temperatureAvailable()) {
  rm3100.SetDrdyIntFlag(0);
     rm3100.ReadRM3100();
     rm3100.DisplayREVIDReg();
-     rm3100.SelfTest();
-#endif     
+//     rm3100.SelfTest();
+#endif  
+ 
+  //wait until data is ready using 1 of two methods (chosen in options at top of code)
+  if(useDRDYPin){ 
+    while(digitalRead(PIN_DRDY) == LOW); //check RDRY pin
+  }
+  else{
+    while((readReg(RM3100_STATUS_REG) & 0x80) != 0x80); //read internal status register
+  }
+
+
+
+  
 // SPI.endTransaction();    
 #if 0
 // #define csPin 10      // SPI CS
@@ -2165,6 +2234,7 @@ Serial.println(temp,HEX);
 #endif
 
 unsigned long counter = 0;
+#if 0
         //Process Drdy Interrupt
 //        if (rm3100.GetDrdyIntFlag())
 //         if ( digitalRead(datardyPin == HIGH ))
@@ -2173,7 +2243,7 @@ unsigned long counter = 0;
            if ( digitalRead(datardyPin) == HIGH )
         {
 //          rm3100.ReadRM3100();
-#if 1
+#if 0
 //          while ( digitalRead(datardyPin != LOW ))  { 
              while ( digitalRead(datardyPin) != LOW )  { 
             delay(1);
@@ -2193,7 +2263,80 @@ unsigned long counter = 0;
             
 //            counter++;
         }
-           }    
+           }
+#endif 
+  //read measurements
+  digitalWrite(PIN_CS, LOW);
+  delay(100);
+  SPI.transfer(0xA4);
+  x2 = SPI.transfer(0xA5);
+  x1 = SPI.transfer(0xA6);
+  x0 = SPI.transfer(0xA7);
+  
+  y2 = SPI.transfer(0xA8);
+  y1 = SPI.transfer(0xA9);
+  y0 = SPI.transfer(0xAA);
+  
+  z2 = SPI.transfer(0xAB);
+  z1 = SPI.transfer(0xAC);
+  z0 = SPI.transfer(0);
+  
+  digitalWrite(PIN_CS, HIGH);
+
+  //special bit manipulation since there is not a 24 bit signed int data type
+  if (x2 & 0x80){
+      x = 0xFF;
+  }
+  if (y2 & 0x80){
+      y = 0xFF;
+  }
+  if (z2 & 0x80){
+      z = 0xFF;
+  }
+
+  //format results into single 32 bit signed value
+  x = (x * 256 * 256 * 256) | (int32_t)(x2) * 256 * 256 | (uint16_t)(x1) * 256 | x0;
+  y = (y * 256 * 256 * 256) | (int32_t)(y2) * 256 * 256 | (uint16_t)(y1) * 256 | y0;
+  z = (z * 256 * 256 * 256) | (int32_t)(z2) * 256 * 256 | (uint16_t)(z1) * 256 | z0;
+
+  //calculate magnitude of results
+  double uT = sqrt(pow(((float)(x)/gain),2) + pow(((float)(y)/gain),2)+ pow(((float)(z)/gain),2));
+
+  //display results
+  Serial.print("Data in counts:");
+  Serial.print("   X:");
+  Serial.print(x);
+  Serial.print("   Y:");
+  Serial.print(y);
+  Serial.print("   Z:");
+  Serial.println(z);
+
+  Serial.print("Data in microTesla(uT):");
+  Serial.print("   X:");
+  Serial.print((float)(x)/gain);
+  Serial.print("   Y:");
+  Serial.print((float)(y)/gain);
+  Serial.print("   Z:");
+  Serial.println((float)(z)/gain);
+
+  //Magnitude should be around 45 uT (+/- 15 uT)
+  Serial.print("Magnitude(uT):");
+  Serial.println(uT);
+  Serial.println();    
+
+//  if ( loop_ctr++ >= 60 ) {
+  // set the cursor first character
+  myDisplay.home();
+  myDisplay.setCursor(0);
+//  myDisplay.print("A3: ");
+  myDisplay.print(uT, DEC);
+  myDisplay.setCursor(6);
+  myDisplay.print("uT");
+//  delay(750);
+  loop_ctr = 0;
+
+//  }
+              
   }
 }  // end main loop()
 
@@ -2273,10 +2416,45 @@ static void Write_Matrix_String(char *string)
 }
 #endif
 
-void dataRDYIRQ() {
-   rm3100.SetDrdyIntFlag(1);
+// void dataRDYIRQ() {
+//   rm3100.SetDrdyIntFlag(1);
+//}
+//addr is the 7 bit value of the register's address (without the R/W bit)
+uint8_t readReg(uint8_t addr){
+  uint8_t data = 0;
+  digitalWrite(PIN_CS, LOW);
+  delay(100);
+  SPI.transfer(addr | 0x80); //OR with 0x80 to make first bit(read/write bit) high for read
+  data = SPI.transfer(0);
+  digitalWrite(PIN_CS, HIGH);
+  return data;
 }
 
+//addr is the 7 bit (No r/w bit) value of the internal register's address, data is 8 bit data being written
+void writeReg(uint8_t addr, uint8_t data){
+  digitalWrite(PIN_CS, LOW); 
+  delay(100);
+  SPI.transfer(addr & 0x7F); //AND with 0x7F to make first bit(read/write bit) low for write
+  SPI.transfer(data);
+  digitalWrite(PIN_CS, HIGH);
+}
+
+//newCC is the new cycle count value (16 bits) to change the data acquisition
+void changeCycleCount(uint16_t newCC){
+  uint8_t CCMSB = (newCC & 0xFF00) >> 8; //get the most significant byte
+  uint8_t CCLSB = newCC & 0xFF; //get the least significant byte
+    
+  digitalWrite(PIN_CS, LOW); 
+  delay(100);
+  SPI.transfer(RM3100_CCX1_REG & 0x7F); //AND with 0x7F to make first bit(read/write bit) low for write
+  SPI.transfer(CCMSB);  //write new cycle count to ccx1
+  SPI.transfer(CCLSB);  //write new cycle count to ccx0
+  SPI.transfer(CCMSB);  //write new cycle count to ccy1
+  SPI.transfer(CCLSB);  //write new cycle count to ccy0
+  SPI.transfer(CCMSB);  //write new cycle count to ccz1
+  SPI.transfer(CCLSB);  //write new cycle count to ccz0
+  digitalWrite(PIN_CS, HIGH);
+}
 
 
 // *eof
